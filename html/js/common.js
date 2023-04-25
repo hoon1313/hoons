8f29be42790ae8703777f34290cfbb14a22cbbeb
hoon <tkdgns757@naver.com>
2023-04-25 19:42:58 +0900

    0425

--- html/js/common.js
+++ html/js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+
+
+
+
     $(".depth2").hide();
 
     /* 1차메뉴 GNB의 li에 hover시, 그 자식요소 children의 depth2가 슬라이드 */
@@ -9,10 +13,15 @@ $(document).ready(function () {
 
     var swiper = new Swiper(".swiper", {
         loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
         pagination: {
           el: ".swiper-pagination",
           type : 'bullets',  // 버튼 종류 설정 'bullets' | 'fraction' | 'progressbar'
 clickable : true,  // 버튼 클릭 여부
         },
       });
+
 });
